c3609b3536eed4a71b13f78493efc6baaca48a7c
vsolari <vsolari@relke.cl>
2024-05-29 21:23:22 -0400

    FIX - Num version

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
                     <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
                         <svg className="bi" width="30" height="24"><use xlinkHref="#bootstrap"></use></svg>
                     </a>
-                    <span className="col-md-10 text-body-secondary">© {new Date().getFullYear()} Apple Store Ltda.</span>
+                    <span className="col-md-10 text-body-secondary">© {new Date().getFullYear()} Apple Store Ltda. v2.0.1</span>
                 
                     <ul className="nav col-md-12 justify-content-end list-unstyled d-flex">
                         <li className="ms-3"><a className="text-body-secondary" href="#"><FaSquareXTwitter /></a></li>
